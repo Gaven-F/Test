5a06f5bbe3cf48ac27e7d704483d7c4890310f3d
GavenF <gavenf2023@outlook.com>
2024-06-10 17:47:35 +0800

    获取Yaml数据流

--- YamlDemo/Program.cs
+++ YamlDemo/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using YamlDotNet.RepresentationModel;
+
+var document = """
+---
+receipt:    Oz-Ware Purchase Invoice
+date:        2007-08-06
+customer:
+    given:   Dorothy
+    family:  Gale
+    
+items:
+    - part_no:   A4786
+      descrip:   Water Bucket (Filled)
+      price:     1.47
+      quantity:  4
+    
+    - part_no:   E1628
+      descrip:   High Heeled "Ruby" Slippers
+      price:     100.27
+      quantity:  1
+    
+bill-to:  &id001
+    street: |
+            123 Tornado Alley
+            Suite 16
+    city:   East Westville
+    state:  KS
+    
+ship-to:  *id001
+    
+specialDelivery:  >
+    Follow the Yellow Brick
+    Road to the Emerald City.
+    Pay no attention to the
+    man behind the curtain.
+...
+""";
+
+
+var stream = new StringReader(document);
+
+var yaml = new YamlStream();
+yaml.Load(stream);
+
+var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+
+foreach (var entry in mapping.Children)
+{
+    Console.WriteLine(((YamlScalarNode)entry.Key).Value);
+}
+
+// List all the items
+var items = (YamlSequenceNode)mapping.Children["items"];
+
+foreach (YamlMappingNode item in items.Cast<YamlMappingNode>())
+{
+    Console.WriteLine(
+        "{0}\t{1}",
+        item.Children[new YamlScalarNode("part_no")],
+        item.Children[new YamlScalarNode("descrip")]
+    );
+}
